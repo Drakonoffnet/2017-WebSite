08d748336724626f185eb4117f1866b7a96c904f
Anton Boyko <boyko.ant@live.com>
2017-07-23 19:06:30 +0300

    started with workshops localization

---
+++ TeamSpark.AzureDay.WebSite.App/Entity/Workshop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TeamSpark.AzureDay.WebSite.App.Entity
+{
+	public class Workshop
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public Language Language { get; set; }
+
+		public Speaker Speaker { get; set; }
+
+		public Workshop()
+		{
+			Language = new Language();
+			Speaker = new Speaker();
+		}
+	}
+}
